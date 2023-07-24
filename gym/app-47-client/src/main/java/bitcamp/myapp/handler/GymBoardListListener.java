992e3b1bfca7c273c92a5c15d7c365a5950936cc
bibibigg <aerl0706@naver.com>
2023-07-24 19:23:42 +0900

    SQL 삽입 공격 차단하기 app-47에 적용

---
+++ gym/app-47-client/src/main/java/bitcamp/myapp/handler/GymBoardListListener.java
@@ -0,0 +1,31 @@
+package bitcamp.myapp.handler;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+import bitcamp.myapp.dao.BoardDao;
+import bitcamp.myapp.vo.Board;
+import bitcamp.util.ActionListener;
+import bitcamp.util.BreadcrumbPrompt;
+
+public class GymBoardListListener implements ActionListener {
+
+  BoardDao boardDao;
+  SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
+
+  public GymBoardListListener(BoardDao boardDao) {
+    this.boardDao = boardDao;
+  }
+
+  public void service(BreadcrumbPrompt prompt) {
+    System.out.println("---------------------------------------");
+    System.out.println("번호, 제목, 작성자, 조회수, 등록일");
+    System.out.println("---------------------------------------");
+
+    List<Board> list = boardDao.list();
+
+    for (Board board : list) {
+      System.out.printf("%d, %s, %s, %d, %s\n", board.getNo(), board.getTitle(), board.getWriter(),
+          board.getViewCount(), dateFormatter.format(board.getCreatedDate()));
+    }
+  }
+}
